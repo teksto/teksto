b47c289c34d8e6a916fca5e9af7786ab5dda2acf
wolfan <wolfan@qq.com>
2018-07-24 19:54:31 +0800

    db-20180724

--- ccamc.py
+++ ccamc.py
@@ -3,9 +3,10 @@
 # 0x187ff
 # 不需要部首部分。
 
-import requests
-import json
 import csv
+import json
+import requests
+from bs4 import BeautifulSoup
 
 # 采集地址 
 url= 'http://www.ccamc.co/tangut.php?unicode='
@@ -14,14 +15,3 @@ url= 'http://www.ccamc.co/tangut.php?unicode='
 u= 0x17000
 # n= 0x187ff
 n= 0x1700f
-
-# 生成文件名
-o0= 'tangut.csv'
-o1= 'tg.json'
-o2= 'tg.yaml'
-
-
-
-# 循环测试
-for i in range(u,n):
-  print (i)
